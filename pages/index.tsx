a26931469a43bef98ac6ae46cde6848d39086f5c
queuebit <queuebit10648@gmail.com>
2023-01-19 09:58:17 -0500

    Fix eslint and Type Error.

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home({
           follow me on <a href="https://mastodon.social/@queuebit">Mastodon</a>.
         </p>
         <p>
-          (This is a sample website - you'll be building a site like this on{" "}
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
